6c75548f3ac705a2ef0455c210370fa5bca2a958
GiuliaBerta <giulia.bertazzini@stud.unifi.it>
2020-09-09 16:53:01 +0200

    add run all test

--- test/runAllTest.cpp
+++ test/runAllTest.cpp
@@ -2,3 +2,9 @@
 // Created by giulia on 08/09/20.
 //
 
+#include "/home/giulia/CLionProjects/IniFile/test/lib/googletest/include/gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
