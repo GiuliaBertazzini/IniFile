71a0e1326e0345d387b28306521c8dccfc5ba598
GiuliaBerta <giulia.bertazzini@stud.unifi.it>
2020-09-11 12:19:16 +0200

    fixed

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include "IniFileManager.h"
+#include "Print.h"
 
 
 int main (){
-    IniFileManager file ("FirstProgram.ini");
+    IniFileManager file ("File.ini");
     std::cout<<"File Name: " << file.getFileName() << std::endl;
+
+    Print p (&file);
 
     file.addSection("Prima Sezione");
     file.addSection("Seconda Sezione");
     file.addParameter("Prima sezione", "Parametro 1");
     file.addParameter("Prima Sezione", "Parametro 2");
+    file.addParameter("Prima Sezione", "Parametro 3");
     file.addParameter("Seconda Sezione", "Parametro 1");
 
-    file.setIntValue("Prima Sezione", "Parametro 1", 10);
-    file.setFloatValue("Prima Sezione", "Parametro 2", 9.81);
-    file.setBoolValue("Seconda Sezione", "Parametro 1", true);
+    file.setValue("Prima Sezione", "Parametro 1", 10);
+    file.setValue("Prima Sezione", "Parametro 2", 9.8);
+    file.setValue("Seconda Sezione", "Parametro 1", true);
     file.setStringValue("Prima Sezione", "Parametro 3", "valore");
 
-    file.checkIsOpen();
-
     file.addSection("Terza Sezione");
-    file.addParameter("Terza", "Parametro 1");
-    file.removeParameter("Prima Sezione","Parametro 2");
-    file.removeSection("Quarta Sezione");
-    file.addComment("primo commento", "secondo commento", false);
+    file.addParameter("Terza Sezione", "Parametro 1");
+    file.addComment("Terza Sezione", "secondo commento", false);
 
-
-    file.printAll();
+    p.printSections();
+    p.printParameters("Prima Sezione");
+    p.printValue("Prima Sezione", "Parametro 1");
+    p.printAll();
 
     int numberOfParameters = file.countParameters("Prima Sezione");
     std::cout<< "Numero parametri della prima sezione: " << numberOfParameters << std::endl;
